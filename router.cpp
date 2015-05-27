9b8bb0b01363bf3d2d5e6b888f1327b45ac92e53
andytheyang <achyang@ucdavis.edu>
2015-05-26 19:59:02 -0700

    transfers and getTransfer

--- router.cpp
+++ router.cpp
@@ -8,12 +8,6 @@ using namespace std;
 
 Router::Router(CityInfo *info, int num) : cities(info), numCities(num)
 {
-  for (int i = 0; i < numCities; i++)
-  {
-    cout << "City " << i << " nets " << cities[i].production - (cities + i)->usage << endl;
-//    (infos + i)->production = (infos + i)->usage;
-  }  // for all cities
-  
 } // Router()
 
 
@@ -21,3 +15,24 @@ int Router::setTransfers(Transfer **transfers)
 {
   return 0;  // should be set to total of all transfers.
 }  // setTransfers
+
+// ---PRIVATE---
+
+void Router::transfer(Transfer **transfers, int from, int toIndex, int amount)
+{
+  Transfer trans = getTransfer(transfers, from, toIndex);
+  trans.amount = amount;
+  // optimize by storing cities[from] first
+  cities[from].production -= amount;
+  cities[cities[from].adjList[toIndex]].production += amount;
+}  // transfer()
+
+Transfer& Router::getTransfer(Transfer **transfers, int from, int toIndex)
+{
+  Transfer adj = transfers[from][toIndex];
+
+  if (adj.destCity == 0 && adj.amount == 0)	// not initialized
+    adj.destCity = cities[from].adjList[toIndex];	// initialize
+
+  return adj;
+}  // getTransfer()
--- router.h
+++ router.h
@@ -10,7 +10,31 @@ public:
 private:
   CityInfo *cities;
   int numCities;
+  void transfer(Transfer **transfers, int from, int toIndex, int amount);	// moves production
+  Transfer& getTransfer(Transfer **transfers, int from, int toIndex);
 }; // class Router 
+
+/*
+class City {
+public:
+  City();
+  void setData(int *aList, short aCount, int use, int prod)
+  {
+    for (int i = 0; i < 8; i++)
+    {
+      adjList[i] = aList[i];
+    }
+
+    adjCount = (int)aCount;
+    usage = use;
+    production = prod;
+  }
+  int adjList[8];
+  int adjCount;		// don't deal with data types
+  int usage;
+  int production;
+};  // class City
+*/
 
 #endif	// ROUTER_H
 
